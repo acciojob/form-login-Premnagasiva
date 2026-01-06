097513a34bd34e7a6e860631579d98395810b8eb
Prem Naga Siva <premnagasiva@gmail.com>
2026-01-06 21:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
-
+	e.preventDefault()
+	const fname = e.target.fname.value;
+	const lname = e.target.lname.value
+	const fullname = `${fname} ${lname}`
+	alert(fullname)
 }
